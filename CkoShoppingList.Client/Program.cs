2504b80a4fe005685ba80a95edeaf0242f2846c0
Ignas Sakalauskas <ISakalauskas@uk.imshealth.com>
2017-06-04 23:17:05 +0100

    Update client to use bearer token. Clean up code.

--- CkoShoppingList.Client/Program.cs
+++ CkoShoppingList.Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Configuration;
 using Checkout;
 using Checkout.ApiServices.ShoppingList.RequestModels;
+using IdentityModel.Client;
 
 namespace CkoShoppingList.Client
 {
@@ -10,39 +12,64 @@ namespace CkoShoppingList.Client
         {
             try
             {
-                // TODO get the actual secret key
-                var ckoApiClient = new APIClient("sk_test_32b9cb39-1cd6-4f86-b750-7069a133667d", Checkout.Helpers.Environment.ShoppingListTest);
+                var tokenHeader = GetBearerTokenHeader();
 
-                Console.WriteLine("[INFO] Making CREATE request.");
-                var resp1 = ckoApiClient.ShoppingListService.CreateDrink(new DrinkCreate
-                {
-                    Name = "Coke",
-                    Quantity = 123
-                });
-
-                Console.WriteLine("[INFO] Making GET request.");
-                var resp2 = ckoApiClient.ShoppingListService.GetDrinkList(new DrinkGetList());
-
-                Console.WriteLine("[INFO] Making GET BY ID request.");
-                var resp3 = ckoApiClient.ShoppingListService.GetDrink("Coke");
-
-                Console.WriteLine("[INFO] Making UPDATE request.");
-                var resp4 = ckoApiClient.ShoppingListService.UpdateDrink("Coke", new DrinkUpdate
-                {
-                    Name = "Coke",
-                    Quantity = 333
-                });
-
-                Console.WriteLine("[INFO] Making DELETE request.");
-                var resp5 = ckoApiClient.ShoppingListService.DeleteDrink("Coke");
+                TestShoppingListEndpoints(tokenHeader);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Exception: {ex.Message}");
             }
 
-            Console.WriteLine("done! any key to boom!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void TestShoppingListEndpoints(string bearerToken)
+        {
+            var ckoApiClient = new APIClient(bearerToken, Checkout.Helpers.Environment.ShoppingListTest);
+
+            Console.WriteLine("[INFO] Making CREATE request.");
+            var resp1 = ckoApiClient.ShoppingListService.CreateDrink(new DrinkCreate
+            {
+                Name = "Coke",
+                Quantity = 123
+            });
+
+            Console.WriteLine("[INFO] Making GET request.");
+            var resp2 = ckoApiClient.ShoppingListService.GetDrinkList(new DrinkGetList());
+
+            Console.WriteLine("[INFO] Making GET BY ID request.");
+            var resp3 = ckoApiClient.ShoppingListService.GetDrink("Coke");
+
+            Console.WriteLine("[INFO] Making UPDATE request.");
+            var resp4 = ckoApiClient.ShoppingListService.UpdateDrink("Coke", new DrinkUpdate
+            {
+                Name = "Coke",
+                Quantity = 333
+            });
+
+            Console.WriteLine("[INFO] Making DELETE request.");
+            var resp5 = ckoApiClient.ShoppingListService.DeleteDrink("Coke");
+
+            Console.WriteLine("[INFO] All tests done!");
+        }
+
+        private static string GetBearerTokenHeader()
+        {
+            var client = new TokenClient(
+                ConfigurationManager.AppSettings["AuthorityUri"],
+                ConfigurationManager.AppSettings["ClientId"],
+                ConfigurationManager.AppSettings["ClientSecret"]);
+
+            var response = client.RequestClientCredentialsAsync(ConfigurationManager.AppSettings["Scope"]).Result;
+            if (response.IsError)
+            {
+                throw new Exception($"Error retrieving access token from identity server: {response.Error}");
+            }
+
+            return $"Bearer {response.AccessToken}";
         }
     }
 }
